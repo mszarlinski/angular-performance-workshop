55b1f8ea9f61a27ab3741f2c619f6ddffe73e60b
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-04-07 14:02:02 +0200

    avoid filters where possible

--- heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
+++ heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
@@ -3,17 +3,4 @@
 angular.module('cardsBrowserModule')
     .controller('CardsBrowserCtrl', function (cards) {
         this.cards = cards;
-    })
-    .filter('rarityToNumber', function() {
-        var RARITY_TO_NUMBER = {
-            FREE: 0,
-            COMMON: 1,
-            RARE: 2,
-            EPIC: 3,
-            LEGENDARY : 4
-        };
-
-        return function(input) {
-            return RARITY_TO_NUMBER[input];
-        }
     });
