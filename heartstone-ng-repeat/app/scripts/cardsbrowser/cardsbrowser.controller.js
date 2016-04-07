5b648dae3f4aa66ff90e8e6cf0c8c34c3de5f61e
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-04-07 15:27:30 +0200

    replace ng-repeat filter with hand-made filter in $watch

--- heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
+++ heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
@@ -1,6 +1,20 @@
 'use strict';
 
 angular.module('cardsBrowserModule')
-    .controller('CardsBrowserCtrl', function (cards) {
+    .controller('CardsBrowserCtrl', function ($scope, cards) {
+        var vm = this;
+
         this.cards = cards;
+        this.displayedCards = angular.copy(cards);
+        this.searchString = '';
+
+        $scope.$watch('vm.searchString', function (val) {
+            if (!val) {
+                vm.displayedCards = angular.copy(cards);
+            } else {
+                vm.displayedCards = _.filter(angular.copy(cards), function (card) {
+                    return card.name.indexOf(val) > -1;
+                });
+            }
+        });
     });
