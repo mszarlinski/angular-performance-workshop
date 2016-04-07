3e681011a8d9b4bc5afa48fd18aa07d891eb7028
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-04-07 13:12:12 +0200

    Bad perf

--- heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
+++ heartstone-ng-repeat/app/scripts/cardsbrowser/cardsbrowser.controller.js
@@ -2,7 +2,26 @@
 
 angular.module('cardsBrowserModule')
     .controller('CardsBrowserCtrl', function (cards) {
-        console.log('cards = ', cards);
         this.cards = cards;
 
+        this.showCost = function (cost) {
+            var result = '';
+            for (var i = 0; i < cost; i++) {
+                result += ' *';
+            }
+            return result;
+        }
+    })
+    .filter('rarityToNumber', function() {
+        var RARITY_TO_NUMBER = {
+            FREE: 0,
+            COMMON: 1,
+            RARE: 2,
+            EPIC: 3,
+            LEGENDARY : 4
+        };
+
+        return function(input) {
+            return RARITY_TO_NUMBER[input];
+        }
     });
