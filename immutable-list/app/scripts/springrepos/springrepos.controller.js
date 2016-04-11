00da98cdb9fe942bf15e38b1d1acc45207e93dff
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-04-11 15:12:20 +0200

    Remove immutable-list subproject

--- immutable-list/app/scripts/springrepos/springrepos.controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('springReposModule')
-    .controller('SpringReposCtrl', function (repos) {
-        console.log('### repos: ', repos);
-        this.repos = repos;
-    });
